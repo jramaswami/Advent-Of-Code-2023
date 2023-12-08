e91765dab1e29b83abc18b28d9b74603ef186682
Joseph Ramaswami <jramaswami@gmail.com>
2023-12-08 05:39:25 -0500

    Solution to second part of day 8 that is too slow

--- python/08/day08.py
+++ python/08/day08.py
@@ -57,6 +57,34 @@ def test_solve_a_llr():
     assert result == 6
 
 
+def solve_b(instructions, graph):
+    # Find all nodes that end in 'A'
+    curr_queue = [n for n in graph if n.endswith('A')]
+    next_queue = []
+    steps = 0
+    i = 0
+    while not all(n.endswith('Z') for n in curr_queue):
+        print(curr_queue)
+        for node in curr_queue:
+            if instructions[i] == 'L':
+                next_queue.append(graph[node].left)
+            else:
+                next_queue.append(graph[node].right)
+        curr_queue, next_queue = next_queue, []
+        steps += 1
+        i += 1
+        i %= len(instructions)
+    return steps
+
+
+def test_solve_b_():
+    with open('../../data/08/test08LR.txt', 'r') as infile:
+        lines = infile.readlines()
+    instructions, graph = parse_input(lines)
+    result = solve_b(instructions, graph)
+    assert result == 6
+
+
 def main():
     "Main program"
     import pyperclip
@@ -67,6 +95,13 @@ def main():
     soln_a = solve_a(instructions, graph)
     print('It takes', soln_a, 'steps to reach ZZZ')
     assert soln_a == 19951
+    soln_b = solve_b(instructions, graph)
+    print(
+        'Starting with all nodes ending in A, it takes',
+        soln_b,
+        'steps to reach all nodes ending in Z'
+    )
+    pyperclip.copy(soln_b)
 
 
 if __name__ == '__main__':
