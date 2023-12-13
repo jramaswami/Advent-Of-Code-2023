e7d8e9ac6f340a947042eec402282a3631cee289
Joseph Ramaswami <jramaswami@gmail.com>
2023-12-13 07:01:06 -0500

    WA for second part of day 13

--- python/13/day13.py
+++ python/13/day13.py
@@ -3,6 +3,8 @@ Advent of Code
 Day 13
 Point of Incidence
 jramaswami
+
+22255 is too low
 """
 
 
@@ -76,6 +78,10 @@ def test_find_mirror():
     grid2_t = rotate_grid(grid2)
     assert find_mirror(grid2_t) == 4
 
+    grid1 = read_input('../../data/13/test13b.txt')[0]
+    grid1_t = rotate_grid(grid1)
+    assert find_mirror(rotate_grid(grid1)) == 1
+
 
 def solve_a(grids):
     rs, cs = 0, 0
@@ -96,6 +102,54 @@ def test_solve_a():
     assert solve_a(grids) == expected
 
 
+def fix_smudge(grid):
+    # Fix smudge
+    grid = [[c for c in row] for row in grid]
+
+    # Find original row/col
+    result = []
+    result.append((find_mirror(grid), find_mirror(rotate_grid(grid))))
+    # Find all smudge fixes
+    for r, row in enumerate(grid):
+        for c, _ in enumerate(row):
+            rs, cs = 0, 0
+            # Swap the character here
+            if grid[r][c] == '.':
+                grid[r][c] = '#'
+            else:
+                grid[r][c] = '.'
+            result.append((find_mirror(grid), find_mirror(rotate_grid(grid))))
+            # Swap the character back
+            if grid[r][c] == '.':
+                grid[r][c] = '#'
+            else:
+                grid[r][c] = '.'
+    return result
+
+
+def solve_b(grids):
+    rs, cs = 0, 0
+    for grid in grids:
+        result = fix_smudge(grid)
+        # Original result is first item
+        cs0, rs0 = result[0]
+        for cs_, rs_ in result[1:]:
+            if rs_ > 0 or cs_ > 0:
+                if rs_ > 0 and rs_ != rs0:
+                    rs += rs_
+                    break
+                if cs_ > 0 and cs_ != cs0:
+                    cs += cs_
+                    break
+    return (100 * rs) + cs
+
+
+def test_solve_b():
+    grids = read_input('../../data/13/test13a.txt')
+    expected = 400
+    assert solve_b(grids) == expected
+
+
 def main():
     "Main program"
     import pyperclip
@@ -103,7 +157,9 @@ def main():
     soln_a = solve_a(grids)
     print('The summary number is', soln_a)
     assert soln_a == 37113
-    pyperclip.copy(str(soln_a))
+    soln_b = solve_b(grids)
+    print('The summary number with new reflection lines is', soln_b)
+    pyperclip.copy(str(soln_b))
 
 
 if __name__ == '__main__':
