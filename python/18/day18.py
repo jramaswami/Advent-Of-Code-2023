c3c10f4a4eef16d58170008badd5b43d09a31c9b
Joseph Ramaswami <jramaswami@gmail.com>
2023-12-18 12:14:23 -0500

    Feeling a bit lost

--- python/18/day18.py
+++ python/18/day18.py
@@ -12,6 +12,7 @@ import math
 
 Vector = collections.namedtuple('Vector', ['row', 'col'])
 Vector.__add__ = lambda a, b: Vector(a.row + b.row, a.col + b.col)
+Vector.__mul__ = lambda a, b: Vector(a.row * b, a.col * b)
 
 
 DigInstruction = collections.namedtuple('DigInstruction', ['dirn', 'meters', 'color'])
@@ -36,7 +37,7 @@ def dig_trench(dig_instructions):
     visited = set()
     posn = Vector(0, 0)
     visited.add(posn)
-    for instruction in dig_instructions:
+    for i, instruction in enumerate(dig_instructions):
         for _ in range(instruction.meters):
             posn = posn + instruction.dirn
             visited.add(posn)
@@ -102,6 +103,7 @@ def trench_volume(trench):
 
 def solve_a(dig_instructions):
     trench = dig_trench(dig_instructions)
+    # print_trench(trench)
     soln_a = trench_volume(trench)
     return soln_a
 
@@ -146,9 +148,116 @@ def solve_b(dig_instructions):
     return soln_b
 
 
-def test_solve_b():
+# def test_solve_b():
+#     dig_instructions = read_input('../../data/18/test18a.txt')
+#     assert solve_b(dig_instructions) == 952408144115
+
+
+HZ = 2
+ADD_VT = 1
+SUB_VT = 3
+
+def solve(dig_instructions):
+    start_posn = Vector(0, 0)
+    curr_posn = start_posn
+    end_posn = None
+    min_row = max_row = min_col = max_col = 0
+    row_events = collections.defaultdict(list)
+    for dig in dig_instructions:
+        next_posn = curr_posn + (dig.dirn * dig.meters)
+        end_posn = next_posn
+        min_row, max_row = min(min_row, next_posn.row), max(max_row, next_posn.row)
+        min_col, max_col = min(min_col, next_posn.col), max(max_col, next_posn.col)
+
+        print(curr_posn, '->', next_posn, ':', dig.dirn)
+        if curr_posn.row == next_posn.row:
+            # Movement was horizontal, just one event
+            left = min(curr_posn.col, next_posn.col)
+            row_events[curr_posn.row].append((left, HZ, dig.meters))
+        else:
+            top, bottom = min(curr_posn.row, next_posn.row), max(curr_posn.row, next_posn.row)
+            row_events[top].append((curr_posn.col, ADD_VT,))
+            row_events[bottom].append((curr_posn.col, SUB_VT))
+        curr_posn = next_posn
+
+    assert start_posn == end_posn
+
+    print('mn/mx row', min_row, max_row, max_row-min_row)
+    print('mn/mx col', min_col, max_col, max_col-min_col)
+
+    for row in sorted(row_events):
+
+        print(row, row_events[row])
+
+    # row_events = sorted(hzs)
+    # curr_hzs = hzs[row_events[0]]
+    # volume = 0
+    # for row in row_events[1:]:
+    #     print(f'{curr_hzs=} {row=} {hzs[row]=}')
+    #     curr_hzs.extend
+
+
+def solve_by_horizontal(dig_instructions):
+    start_posn = Vector(0, 0)
+    curr_posn = start_posn
+    end_posn = None
+    min_row = max_row = min_col = max_col = 0
+    row_events = collections.defaultdict(list)
+    for dig in dig_instructions:
+        next_posn = curr_posn + (dig.dirn * dig.meters)
+        end_posn = next_posn
+        min_row, max_row = min(min_row, next_posn.row), max(max_row, next_posn.row)
+        min_col, max_col = min(min_col, next_posn.col), max(max_col, next_posn.col)
+        print(curr_posn, '->', next_posn, ':', dig.dirn)
+        if curr_posn.row == next_posn.row:
+            # Movement was horizontal, just one event
+            left = min(curr_posn.col, next_posn.col)
+            row_events[curr_posn.row].append((left, left + dig.meters))
+        curr_posn = next_posn
+
+    assert start_posn == end_posn
+
+    print('mn/mx row', min_row, max_row, max_row-min_row)
+    print('mn/mx col', min_col, max_col, max_col-min_col)
+
+    curr_intervals = []
+    prev_row = None
+    for row in sorted(row_events):
+        print(curr_intervals)
+        t = list(curr_intervals)
+        t.extend(row_events[row])
+        t.sort()
+        next_intervals = [t[0]]
+        for x in t[1:]:
+            if next_intervals[-1][1] == x[0]:
+                # Combine intervals if the overlap by 1
+                z = (next_intervals[-1][0], t[1])
+                next_intervals.pop()
+                next_intervals.append(z)
+            elif
+                # Subtract from previous interval.
+
+
+
+def f(dig_instructions):
+    volume = 0
+    curr_posn = Vector(0, 0)
+    perimeter = 0
+    for i, instruction in enumerate(dig_instructions):
+        next_posn = curr_posn + (instruction.dirn * instruction.meters)
+        volume += (curr_posn.col - next_posn.col) * (curr_posn.row + next_posn.row)
+        perimeter += instruction.meters
+        curr_posn = next_posn
+    print(perimeter, volume - 62)
+    return volume // 2
+
+
+
+
+def test_solve():
     dig_instructions = read_input('../../data/18/test18a.txt')
-    assert solve_b(dig_instructions) == 952408144115
+    assert solve_by_horizontal(dig_instructions) == 62
+
 
 
 def main():
@@ -161,6 +270,9 @@ def main():
     assert soln_a == 39194
     pyperclip.copy(str(soln_a))
 
+    t = translate_dig_instructions(dig_instructions)
+    return
+    soln_b = solve()
 
 if __name__ == '__main__':
     main()
