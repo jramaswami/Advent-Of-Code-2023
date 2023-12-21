fb2ebd3786fd3726b0093d3443d7c22eef30833b
Joseph Ramaswami <jramaswami@gmail.com>
2023-12-21 10:04:03 -0500

    Intermediate commit

--- python/21/day21.py
+++ python/21/day21.py
@@ -58,6 +58,89 @@ def test_solve_a():
     assert solve_a(grid, 6) == 16
 
 
+def manhattan_distance(p, q):
+    return abs(p.row - q.row) + abs(p.col - q.col)
+
+
+def solve_b(grid, ticks):
+    visited = dict()
+    queue = collections.deque()
+    start = find_start(grid)
+    visited[start] = 0
+    queue.append(start)
+    min_row, max_row = -(7 * len(grid)), (7 * len(grid))
+    min_col, max_col = - (7 * len(grid[0])), (7 * len(grid[0]))
+
+    print(min_row, max_row)
+    print(min_col, max_col)
+    print(start)
+    while queue:
+        p = queue.popleft()
+        for d in OFFSETS:
+            q = p + d
+            if q.row < min_row or q.row >= max_row:
+                continue
+            if q.col < min_col or q.col >= max_col:
+                continue
+
+            if q in visited:
+                continue
+            if grid[q.row % len(grid)][q.col % len(grid[0])] == '#':
+                continue
+
+            visited[q] = visited[p] + 1
+            queue.append(q)
+
+    grids_seen = dict()
+    gog = []
+    dog = []
+    for r_ in range(-5, 5+1):
+        curr_gog = []
+        curr_dog = []
+        for c_ in range(-5, 5+1):
+            dr = len(grid) * r_
+            dc = len(grid) * c_
+
+            curr_grid = []
+            for r in range(dr, dr+len(grid)):
+                grid_row = []
+                for c in range(dc, dc+len(grid[0])):
+                    p = Vector(r, c)
+                    if p in visited:
+                        grid_row.append(str(visited[p] % 2))
+                    else:
+                        grid_row.append('#')
+                curr_grid.append(''.join(grid_row))
+
+            curr_grid = '\n'.join(row for row in curr_grid)
+            # print(curr_grid)
+            if curr_grid not in grids_seen:
+                grids_seen[curr_grid] = len(grids_seen)
+            curr_gog.append(grids_seen[curr_grid])
+            curr_dog.append(visited[Vector(dr+start.row, dc+start.col)])
+        gog.append(curr_gog)
+        dog.append(curr_dog)
+
+    for row in gog:
+        print(row)
+
+    for row in dog:
+        print(row)
+
+    # for r in range(min_row+start.row, max_row, len(grid)):
+    #     for d in (0,):
+    #         for c in range(min_col+start.col, max_col, len(grid[0])):
+
+    #                 p = Vector(r+d,c)
+    #                 if p in visited:
+    #                     print(p, visited[p], end=', ')
+    #                 else:
+    #                     print(p, '#', end=', ')
+    #         print()
+
+
+
+
 def main():
     "Main program"
     import pyperclip
@@ -67,6 +150,8 @@ def main():
     assert soln_a == 3649
     pyperclip.copy(str(soln_a))
 
+    grid = read_input('../../data/21/test21a.txt')
+    soln_b = solve_b(grid, 10000)
 
 if __name__ == '__main__':
     main()
