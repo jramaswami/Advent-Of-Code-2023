ae0d7cfbfa85e746023fc2829b54a37378b01de1
Joseph Ramaswami <jramaswami@gmail.com>
2023-12-21 13:33:03 -0500

    WA

--- python/21/day21.py
+++ python/21/day21.py
@@ -3,10 +3,51 @@ Advent of Code
 Day 21
 Step Counter
 jramaswami
+
+For part b, inspection of puzzle input shows a repeating pattern of parity grids
+
+[0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0]
+[1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1]
+[0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0]
+[1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1]
+[0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0]
+[1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1]
+[0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0]
+[1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1]
+[0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0]
+[1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1]
+[0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0]
+
+Where there are only two kinds of parity grids.
+
+There is also a pattern in the distances to the center of the a grid section:
+
+[1310, 1179, 1048, 917, 786, 655, 786, 917, 1048, 1179, 1310]
+[1179, 1048, 917, 786, 655, 524, 655, 786, 917, 1048, 1179]
+[1048, 917, 786, 655, 524, 393, 524, 655, 786, 917, 1048]
+[917, 786, 655, 524, 393, 262, 393, 524, 655, 786, 917]
+[786, 655, 524, 393, 262, 131, 262, 393, 524, 655, 786]
+[655, 524, 393, 262, 131, 0, 131, 262, 393, 524, 655]
+[786, 655, 524, 393, 262, 131, 262, 393, 524, 655, 786]
+[917, 786, 655, 524, 393, 262, 393, 524, 655, 786, 917]
+[1048, 917, 786, 655, 524, 393, 524, 655, 786, 917, 1048]
+[1179, 1048, 917, 786, 655, 524, 655, 786, 917, 1048, 1179]
+[1310, 1179, 1048, 917, 786, 655, 786, 917, 1048, 1179, 1310]
+
+Note that delta is 131.  So the distance to the center of
+subgrid[r][c] = delta * (|r| + |c|) and the type of grid
+is gridtype[(|r|+|c|) % 2]
+
+This does not work for the test input!
+
+
+128506022 is too low
+10518681788970528282 is too high
 """
 
 
 import collections
+import math
 
 
 Vector = collections.namedtuple('Vector', ['row', 'col'])
@@ -62,49 +103,127 @@ def manhattan_distance(p, q):
     return abs(p.row - q.row) + abs(p.col - q.col)
 
 
-def solve_b(grid, ticks):
+def solve_b(grid):
+    ticks = 26501365
+
+    # Collect grid types
+    visited = dict()
+    queue = collections.deque()
     start = find_start(grid)
-    visited = set()
-    visited.add(start)
-    parity = ticks % 2
-    print('looking for parity', parity)
-    soln_b = 0
-    curr_queue = []
-    curr_queue.append(start)
-    next_queue = []
-    for curr_dist in range(ticks):
-        if curr_dist % 2 == parity:
-            soln_b += len(curr_queue)
-        for p in curr_queue:
-            for d in OFFSETS:
-                p0 = p + d
-                if grid[p0.row % len(grid)][p0.col % len(grid[0])] == '#':
-                    continue
-                if p0 in visited:
-                    continue
-                next_queue.append(p0)
-                visited.add(p0)
+    visited[start] = 0
+    queue.append(start)
+    min_row, max_row = -(7 * len(grid)), (7 * len(grid))
+    min_col, max_col = - (7 * len(grid[0])), (7 * len(grid[0]))
+    while queue:
+        p = queue.popleft()
+        for d in OFFSETS:
+            q = p + d
+            if q.row < min_row or q.row >= max_row:
+                continue
+            if q.col < min_col or q.col >= max_col:
+                continue
 
-        curr_queue, next_queue = next_queue, []
-    return soln_b + len(curr_queue)
+            if q in visited:
+                continue
+            if grid[q.row % len(grid)][q.col % len(grid[0])] == '#':
+                continue
+
+            visited[q] = visited[p] + 1
+            queue.append(q)
+
+    grids_seen = dict()
+    grid_id = dict()
+    for r_ in range(-5, 5+1):
+        for c_ in range(-5, 5+1):
+            dr = len(grid) * r_
+            dc = len(grid) * c_
+
+            curr_grid = []
+            for r in range(dr, dr+len(grid)):
+                grid_row = []
+                for c in range(dc, dc+len(grid[0])):
+                    p = Vector(r, c)
+                    if p in visited:
+                        grid_row.append(str(visited[p] % 2))
+                    else:
+                        grid_row.append('#')
+                curr_grid.append(''.join(grid_row))
+
+            curr_grid = '\n'.join(row for row in curr_grid)
+            if curr_grid not in grids_seen:
+                grids_seen[curr_grid] = len(grids_seen)
+            grid_id[Vector(r_, c_)] = grids_seen[curr_grid]
+
+    assert len(grids_seen) == 2
+
+    print('Distances to center of subgrid')
+    for row_offset in range(-5, 6):
+        r = start.row + (len(grid) * row_offset)
+        for col_offset in range(-5, 6):
+            c = start.col + (len(grid[0]) * col_offset)
+            p = Vector(r,c)
+            if p in visited:
+                x = visited[p]
+                print(f'{x:04}', end=' ')
+            else:
+                print('....', end=' ')
+        print()
+
+    print('Grid type of subgrid')
+    for r in range(-5, 6):
+        for c in range(-5, 6):
+            p = Vector(r,c)
+            print(grid_id[p], end=' ')
+        print()
+
+    # What grids can be reached?
+    DELTA = 131
+
+    # print(ticks / 131)
+    # # How many grids can be reached
+    # grid_reachables = [g.count('1') for g in grids_seen]
+    # reachable_grids = int(math.ceil(2 * ticks / 131))
+    # print('reachable grids', reachable_grids)
+    # ones_zeros_row = [1 + ((reachable_grids-1) // 2), ((reachable_grids-1) // 2)]
+    # ones_zeros_total = [131 * x for x in ones_zeros_row]
+    # print('gr', grid_reachables)
+    # print('10r', ones_zeros_row)
+    # print('10t', ones_zeros_total)
+    # ones_zeros_reachable = [x * y for x, y in zip(ones_zeros_total, grid_reachables)]
+    # print(ones_zeros_reachable)
+    # return sum(ones_zeros_reachable)
+
+    XS = [g.count('1') for g in grids_seen]
+    soln_b = XS[0]
+    m = 4
+    for t in range(1, ticks):
+        soln_b += (m * XS[t%2])
+        m += 4
+    soln_b += (m * XS[ticks%2] // 2)
+    return soln_b
 
 
-def test_soln_b():
-    grid = read_input('../../data/21/test21a.txt')
-    assert solve_b(grid, 6) == 16
-    assert solve_b(grid, 10) == 50
-    assert solve_b(grid, 5000) == 16733044
+
+# def test_soln_b():
+#     grid = read_input('../../data/21/test21a.txt')
+#     assert solve_b(grid, 6) == 16
+#     assert solve_b(grid, 10) == 50
+#     assert solve_b(grid, 5000) == 16733044
 
 
 def main():
     "Main program"
     import pyperclip
     grid = read_input('../../data/21/input21.txt')
-    soln_a = solve_a(grid, 64)
-    print('The elf can reach', soln_a, 'garden plots in 64 steps')
-    assert soln_a == 3649
-    pyperclip.copy(str(soln_a))
+    # soln_a = solve_a(grid, 64)
+    # print('The elf can reach', soln_a, 'garden plots in 64 steps')
+    # assert soln_a == 3649
+    # pyperclip.copy(str(soln_a))
 
+    # grid = read_input('../../data/21/test21a.txt')
+    soln_b = solve_b(grid)
+    print('Solution b', soln_b)
+    pyperclip.copy(str(soln_b))
 
 
 if __name__ == '__main__':
